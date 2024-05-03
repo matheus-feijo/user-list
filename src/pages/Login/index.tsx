26494c8e38eba5a20b88056a1bda262f7738cff2
Matheus Feijó <mat.feijo2506@gmail.com>
2024-05-02 20:37:46 -0400

    FEAT/ mensagem de erro ao tentar logar e n encontrar cadastro

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -15,6 +15,7 @@ import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 import { apiService } from "../../services/api";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { Notification } from "../../components/Notification";
 
 interface ILoginFormValues {
   email: string;
@@ -25,6 +26,7 @@ export function Login() {
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm<ILoginFormValues>();
   const [showPassword, setShowPassword] = useState(false);
+  const [isOpenNotification, setIsOpenNotification] = useState(false);
 
   const { data: userList } = useQuery({
     initialData: [],
@@ -40,7 +42,7 @@ export function Login() {
     const userSelected = userList?.find((user) => user.email === email);
 
     if (!userSelected || userSelected?.senha !== password) {
-      alert("Usuário ou Senha incorretas");
+      setIsOpenNotification(true);
       return;
     }
     //Tratando o id como o token da pessoa para poder fazer as requisiçoes necessarias
@@ -53,13 +55,21 @@ export function Login() {
   return (
     <div className={styles.container}>
       <form onSubmit={handleSubmit(onSubmit)} className={styles.card}>
-        <TextField label="Email" variant="standard" {...register("email")} />
+        <TextField
+          label="Email"
+          variant="standard"
+          {...register("email", {
+            required: true,
+          })}
+        />
         <FormControl variant="standard">
           <InputLabel htmlFor="standard-adornment-password">
             Password
           </InputLabel>
           <Input
-            {...register("password")}
+            {...register("password", {
+              required: true,
+            })}
             type={showPassword ? "text" : "password"}
             endAdornment={
               <InputAdornment position="end">
@@ -78,6 +88,13 @@ export function Login() {
           Entrar
         </ButtonCSS>
       </form>
+
+      <Notification
+        isOpen={isOpenNotification}
+        onClose={() => setIsOpenNotification(false)}
+        type="error"
+        message="Cadastro não encontrado"
+      />
     </div>
   );
 }
